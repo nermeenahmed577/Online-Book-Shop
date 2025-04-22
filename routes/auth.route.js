34ea69b5f3a50761c0ded969a86e192472fe9a49
marimreda777 <marimreda777@gmail.com>
2025-04-22 18:11:22 +0000

    Validate-login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,28 @@
 const router = require("express").Router();
 const bodyParser = require("body-parser");
+const check = require("express-validator").check;
+
 
 const authController = require ("../controllers/auth.controller");
 
 router.get("/login",authController.getLogin);
+
 router.post(
     "/login",
     bodyParser.urlencoded({extended: true}),
+    check("email")
+        .not()
+        .isEmpty()
+        .withMessage("email is required")
+        .isEmail()
+        .withMessage("invalid format"),
+    check("password")
+        .not()
+        .isEmpty()
+        .withMessage("password is required")
+        .isLength({ min: 6 })
+        .withMessage("password must be at least 6 charachters"),  
+        
     authController.postLogin
 );
 
