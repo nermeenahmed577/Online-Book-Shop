95bf4463f3cd7478cf845ce1ddfc447b3e58c676
bassant ehab saber esmail <85416647+basant20415@users.noreply.github.com>
2025-04-28 13:55:57 +0300

    Update orders.route.js

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const orderController = require("../controllers/order.constroller");
+const orderController = require("../controllers/order.controller");
 const authGuard = require("./guards/auth.guard");
 
 router.get("/verify-order", authGuard.isAuth, orderController.getOrderVerify);
